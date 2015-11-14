e329ee649f969980d345a1f2a1ab5d9576251d1e
Mykhailo <kobvel@gmail.com>
2015-11-14 23:30:45 +0200

    Execution contexts

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
-var a;
+function b() {
+    console.log('Called b!');
+}
+
+b();
+
+console.log(a);
+
+var a = 'Hello World!';
+
 console.log(a);
 
 
-if (a === undefined) {
-    console.log('a is undefined!');   
-}
-else {
-    console.log('a is defined!');   
-}
+
+
