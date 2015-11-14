fcb3d30ebfcec9d1f95340f948cbf811cf182757
Mykhailo <kobvel@gmail.com>
2015-11-14 23:58:26 +0200

    Boolean existing

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-// long running function
-function waitThreeSeconds() {
-    var ms = 3000 + new Date().getTime();
-    while (new Date() < ms){}
-    console.log('finished function');
+var a;
+
+// goes to internet and looks for a value
+
+if (a) {
+    console.log('Something is there.');
 }
-
-function clickHandler() {
-    console.log('click event!');   
-}
-
-// listen for the click event
-document.addEventListener('click', clickHandler);
-
-
-waitThreeSeconds();
-console.log('finished execution');
